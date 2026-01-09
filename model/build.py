c7edd3374127b66ea35f54cb613e7086eb028ebd
Tedy-su <1604032249@qq.com>
2026-01-09 19:21:50 +0800

    1

--- model/build.py
+++ model/build.py
@@ -147,7 +147,11 @@ class IRRA(nn.Module):
         i_feats = safl_i_feats + i_feats
         return i_feats
 
-    def encode_image_crfm(self, image):
+    def encode_image_base(self, image):
+        x = self.base_model.encode_image(image)
+        return x[:, 0, :].float()
+
+    def encode_image_safl(self, image):
         x = self.base_model.encode_image(image)
         safl_image_feats = self.safl(x[:, 1:, :])
         i_feats = x[:, 0, :].float()
@@ -172,7 +176,11 @@ class IRRA(nn.Module):
         t_feats = safl_t_feats + t_feats
         return t_feats
 
-    def encode_text_crfm(self, text):
+    def encode_text_base(self, text):
+        x = self.base_model.encode_text(text)
+        return x[torch.arange(x.shape[0]), text.argmax(dim=-1)].float()
+
+    def encode_text_safl(self, text):
         x = self.base_model.encode_text(text)
         safl_text_feats = self.safl(x)
         t_feats = x[torch.arange(x.shape[0]), text.argmax(dim=-1)].float()
