23d1fb047203a5276304e05bf5300a6ce52ef74c
SJJ <1604032249@qq.com>
2026-05-08 21:06:49 +0800

    1

--- run.sh
+++ run.sh
@@ -1,6 +1,31 @@
+##Test
 CUDA_VISIBLE_DEVICES=0 \
 python test.py \
---config_file '/media/jqzhu/哈斯提·基拉/UniMoESE/logs/CUHK-PEDES/sdm+itc+aux_cnum9/configs.yaml'
+--config_file './your path/logs/CUHK-PEDES/sdm+itc+aux_cnum9/configs.yaml'
+
+
+##Trainging
+#CUDA_VISIBLE_DEVICES=5 \
+#python train.py \
+#--name php \
+#--img_aug \
+#--batch_size 64 \
+#--loss_names 'sdm+itc+aux' \
+#--dataset_name 'CUHK-PEDES' \
+#--root_dir "./your path/data" \
+#--num_epoch 60 \
+#--num_experts 4 \
+#--topk 2 \
+#--reduction 8 \
+#--moe_layers 4 \
+#--moe_heads 8 \
+#--transformer_lr_factor 1.0 \
+#--moe_lr_factor 2.0 \
+#--aux_factor 3.0 \
+#--lr 3e-6 \
+#--cnum 9
+
+
 
 #ICFG-PEDES CUHK-PEDES RSTPReid
 
